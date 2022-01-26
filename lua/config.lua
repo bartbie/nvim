c81d9784db11293e2939b45518197e65ec5a4e8e
bart0709 <bart0709@stud.kea.dk>
2022-01-26 19:16:29 +0100

    added indent settings, will be moved to sep. file

--- lua/config.lua
+++ lua/config.lua
@@ -4,4 +4,12 @@ local cmd = vim.cmd
 o.number = true
 o.relativenumber = true
 o.cursorline = true
+o.smartcase = true
+o.scrolloff = 1
 o.termguicolors = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
+o.shiftwidth = 4
+o.smartindent = true
