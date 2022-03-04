0cd49252302d5f924052b49bb65089fd0955dc13
bart0709 <bart0709@stud.kea.dk>
2022-03-04 11:21:37 +0100

    redone map function in keymapping.lua

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -1,16 +1,20 @@
-local map = vim.api.nvim_set_keymap
+local function map(mode, lhs, rhs, opts)
+    opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
 
+map('n','<space>','<Nop>')
 vim.g.mapleader = ' '
 
 -- CHADTREE
-map('n', '<leader>n', '<CMD>CHADopen<CR>', {silent=true})
+map('n', '<leader>n', '<CMD>CHADopen<CR>')
 
 -- FUGITIVE
-map('n', '<leader>gg', '<CMD>Git<CR>', {noremap=true})
-map('n', '<leader>gc', '<CMD>Git commit<CR>', {noremap=true})
+map('n', '<leader>gg', '<CMD>Git<CR>')
+map('n', '<leader>gc', '<CMD>Git commit<CR>')
 
 -- TELESCOPE
-map('n', '<leader>ff', '<CMD>Telescope find_files<CR>', {noremap=true})
-map('n', '<leader>fg', '<CMD>Telescope live_grep<CR>', {noremap=true})
-map('n', '<leader>fb', '<CMD>Telescope buffers<CR>', {noremap=true})
-map('n', '<leader>fh', '<CMD>Telescope help_tags<CR>', {noremap=true})
+map('n', '<leader>ff', '<CMD>Telescope find_files<CR>')
+map('n', '<leader>fg', '<CMD>Telescope live_grep<CR>')
+map('n', '<leader>fb', '<CMD>Telescope buffers<CR>')
+map('n', '<leader>fh', '<CMD>Telescope help_tags<CR>')
