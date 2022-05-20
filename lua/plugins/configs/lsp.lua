112e7560d1f937b5a0dfedab0f2ccb0c920f4e5b
bart0709 <bart0709@stud.kea.dk>
2022-05-20 15:51:18 +0200

    added pyrightconfig json, refactored lsp.lua to read it

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -1,15 +1,28 @@
--- Servers with default setups
-local default = {"pylsp", "emmet_ls", "html", "jsonls", "jdtls", "vimls", "zk", "grammarly", "sqlls"}
+local api = vim.api
+local json = vim.json
+local u = require("core.utils")
 
 -- Make runtime files discoverable to the server
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
+-- Path to json folder with server settings
+local configs_path = u.bin_path .. "/lsp_servers_configs/"
+
+-- function to read and decode json server settings
+local function read(filename)
+    return json.decode(u.read_file(configs_path .. filename .. ".json"))
+end
+
+
+-- Servers with default setups
+local default = {"pylsp", "emmet_ls", "html", "jsonls", "jdtls", "vimls", "zk", "grammarly", "sqlls"}
+
 -- Rest of servers
 local servers = {
 
-    pyright = { },
+    pyright = read("pyrightconfig"),
 
     rust_analyzer = {
     },
@@ -28,7 +41,7 @@ local servers = {
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file('', true),
+                library = api.nvim_get_runtime_file('', true),
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
@@ -50,7 +63,6 @@ require("nvim-lsp-installer").setup{
 
 local lsp = require("lspconfig")
 local coq = require("coq")
-
 
 -- -- Default on_attach settings
 -- local function on_attach(client, buffer)
