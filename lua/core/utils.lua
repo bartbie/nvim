8541470f4fe271b7184b1543050ba4bccf89d57d
bart0709 <bart0709@stud.kea.dk>
2022-05-20 13:16:26 +0200

    added read functions to utils, refactored a bit

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,45 +1,65 @@
-M = {}
----The file system path separator for the current platform.
-M.path_separator = "/"
-M.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1
-if M.is_windows == true then
-  M.path_separator = "\\"
+local utils = {}
+
+-- The file system path separator for the current platform.
+utils.path_separator = "/"
+utils.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1
+if utils.is_windows == true then utils.path_separator = "\\" end
+
+
+-- Joins arbitrary number of paths together.
+-- @param ... string The paths to join.
+-- @return string
+utils.join_paths = function(...)
+    local args = {...}
+    if #args == 0 then
+        return ""
+    end
+
+    local all_parts = {}
+    if type(args[1]) =="string" and args[1]:sub(1, 1) == utils.path_separator then
+        all_parts[1] = ""
+    end
+
+    for _, arg in ipairs(args) do
+        local arg_parts = utils.split(arg, utils.path_separator)
+        vim.list_extend(all_parts, arg_parts)
+    end
+    return table.concat(all_parts, utils.path_separator)
 end
 
----Split string into a table of strings using a separator.
----@param inputString string The string to split.
----@param sep string The separator to use.
----@return table table A table of strings.
-M.split = function(inputString, sep)
-  local fields = {}
 
-  local pattern = string.format("([^%s]+)", sep)
-  local _ = string.gsub(inputString, pattern, function(c)
-    fields[#fields + 1] = c
-  end)
-
-  return fields
+-- see if the file exists
+-- @param path string path to file
+-- @return boolean
+utils.file_exists = function(path)
+    local f = io.open(path, "rb")
+    if f then f:close() end
+    return f ~= nil
 end
 
----Joins arbitrary number of paths together.
----@param ... string The paths to join.
----@return string
-M.join_paths = function(...)
-  local args = {...}
-  if #args == 0 then
-    return ""
-  end
 
-  local all_parts = {}
-  if type(args[1]) =="string" and args[1]:sub(1, 1) == M.path_separator then
-    all_parts[1] = ""
-  end
-
-  for _, arg in ipairs(args) do
-    arg_parts = M.split(arg, M.path_separator)
-    vim.list_extend(all_parts, arg_parts)
-  end
-  return table.concat(all_parts, M.path_separator)
+-- get all lines from a file, returns an empty
+-- @param path string path to file
+-- @return table if the file does not exist
+utils.read_lines = function(path)
+    if not utils.file_exists(path) then return {} end
+    local lines = {}
+    for line in io.lines(path) do
+        lines[#lines + 1] = line
+    end
+    return lines
 end
 
-return M
+
+-- Joins arbitrary number of paths together.
+-- @param path string path to file
+-- @return string
+utils.read_file = function(path)
+    local file = io.open(path, "rb") -- r read mode and b binary mode
+    if not file then return nil end
+    local content = file:read "*a" -- *a or *all reads the whole file
+    file:close()
+    return content
+end
+
+return utils
