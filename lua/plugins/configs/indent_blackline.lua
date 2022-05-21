08b64398f9fcedbd42445271e1e60ab4fe95942a
bart0709 <bart0709@stud.kea.dk>
2022-05-22 00:25:42 +0200

    added indent_blackline

--- lua/plugins/configs/indent_blackline.lua
+++ lua/plugins/configs/indent_blackline.lua
@@ -6,8 +6,21 @@ opt.listchars:append("eol:↴")
 
 
 require("indent_blankline").setup {
+    use_treesitter = true,
     show_end_of_line = true,
     show_current_context = true,
     show_current_context_start = true,
-    space_char_blankline = " ",
+    -- char_blankline = '┆',
+    -- space_char_blankline = "",
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = false,
+    filetype_exclude = {
+        "lspinfo",
+        "packer",
+        "checkhealth",
+        "help",
+        "man",
+        "fugitive",
+        "",
+    },
 }
