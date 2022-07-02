7a5053bfc2d1782387725730bca6852dddb60587
bart0709 <bart0709@stud.kea.dk>
2022-07-02 18:21:14 +0200

    feat(DAP): added DAP and companion plugins

---
+++ lua/plugins/configs/dap.lua
@@ -0,0 +1,50 @@
+local dap = require("dap")
+local adapters = dap.adapters
+local config = dap.configurations
+
+local env = function()
+    local variables = {}
+    for k, v in pairs(vim.fn.environ()) do
+        table.insert(variables, string.format("%s=%s", k, v))
+    end
+    return variables
+end
+
+adapters.lldb = {
+    type = "executable",
+    command = "/usr/bin/lldb-vscode", -- adjust as needed, must be absolute path
+    name = "lldb",
+}
+
+config.cpp = {
+    {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+        env = env,
+
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
+    },
+}
+
+-- If you want to use this for Rust and C, add something like this:
+
+config.c = config.cpp
+config.rust = config.cpp
--- lua/plugins/pluginList.lua
+++ lua/plugins/pluginList.lua
@@ -204,6 +204,24 @@ local pluginList = {
             require("plugins.configs.alpha")
         end,
     },
+
+    dap = {
+        "mfussenegger/nvim-dap",
+
+        {
+            "rcarriga/nvim-dap-ui",
+            config = function()
+                require("dapui").setup()
+            end,
+        },
+
+        {
+            "theHamsta/nvim-dap-virtual-text",
+            config = function()
+                require("nvim-dap-virtual-text").setup()
+            end,
+        },
+    },
 }
 
 return pluginList
