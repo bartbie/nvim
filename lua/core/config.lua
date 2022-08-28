0b6b8cc709e161187baf1c190ebbbaeefa9f6d19
bartbie <bartbie37@gmail.com>
2022-08-28 20:45:44 +0200

    docs(core): added comments

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -1,6 +1,8 @@
 local o = vim.o
 local g = vim.g
 local cmd = vim.cmd
+
+-- general
 o.number = true
 o.relativenumber = true
 o.cursorline = true
@@ -9,13 +11,16 @@ o.scrolloff = 1
 o.termguicolors = true
 o.mouse = "a"
 
+-- indent
 o.tabstop = 4
 o.softtabstop = 4
 o.expandtab = true
 o.shiftwidth = 4
 o.smartindent = true
 
+-- status
 o.laststatus = 3
 
+-- quicker loading of filetypes
 g.do_filetype_lua = 1
 g.did_load_filetypes = 0
