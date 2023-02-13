a2cb0f0cb08e1fe7d7a9eb2916b7c52f14c99922
bartbie <bartbie37@gmail.com>
2023-02-13 20:23:00 +0100

    added lsp and cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -135,4 +135,53 @@ return {
             end
         end,
     },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
+        dependencies = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" }, -- Required
+            { "williamboman/mason.nvim" }, -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "hrsh7th/cmp-buffer" }, -- Optional
+            { "hrsh7th/cmp-path" }, -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" }, -- Optional
+
+            -- Snippets
+            { "L3MON4D3/LuaSnip" }, -- Required
+            { "rafamadriz/friendly-snippets" }, -- Optional
+        },
+        opts = {
+            ensure_installed = {
+                "vimls",
+                "grammarly",
+                "lua_ls",
+                "rust_analyzer",
+                "pyright",
+                "eslint",
+                "tsserver",
+                "tailwindcss",
+                "html",
+                "jsonls",
+            },
+        },
+        config = function(_, opts)
+            local lsp = require("lsp-zero").preset({
+                name = "minimal",
+                set_lsp_keymaps = true,
+                manage_nvim_cmp = true,
+                suggest_lsp_servers = false,
+            })
+
+            -- (Optional) Configure lua language server for neovim
+            lsp.nvim_workspace()
+            lsp.ensure_installed(opts.ensure_installed)
+            lsp.setup()
+        end,
+    },
 }
