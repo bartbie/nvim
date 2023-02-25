3334998e6412ebe62ecf7addd3f58b71167ef06c
bartbie <bartbie37@gmail.com>
2023-02-25 22:26:49 +0100

    added leap.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -426,4 +426,33 @@ return {
             })
         end,
     },
+    {
+        "ggandor/flit.nvim",
+        keys = function()
+            local ret = {}
+            for _, key in ipairs({ "f", "F", "t", "T" }) do
+                ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+            end
+            return ret
+        end,
+        opts = { labeled_modes = "nx" },
+    },
+    {
+        "ggandor/leap.nvim",
+        keys = {
+            { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+            { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+            { "s", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+            { "<leader>ss", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+        },
+        config = function(_, opts)
+            local leap = require("leap")
+            for k, v in pairs(opts) do
+                leap.opts[k] = v
+            end
+            leap.add_default_mappings()
+            vim.keymap.del({ "x", "o" }, "x")
+            vim.keymap.del({ "x", "o" }, "X")
+        end,
+    },
 }
