0f095d5a2ef3fb24257a7718f8fe2cb4b60c6485
bartbie <bartbie37@gmail.com>
2023-02-27 11:28:45 +0100

    moved plugins into separate folder

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,6 @@
 local lib = require("lib")
 local icons = lib.diagnostics_symbols
+
 return {
     {
         "folke/tokyonight.nvim",
@@ -128,66 +129,6 @@ return {
             for _, v in ipairs(extensions) do
                 ts.load_extension(v)
             end
-        end,
-    },
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        branch = "v1.x",
-        keys = {
-            { "<leader>m", "<CMD>Mason<CR>", desc = "Mason" },
-        },
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" }, -- Required
-            { "williamboman/mason.nvim" }, -- Optional
-            { "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" }, -- Required
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "hrsh7th/cmp-buffer" }, -- Optional
-            { "hrsh7th/cmp-path" }, -- Optional
-            { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" }, -- Optional
-            { "hrsh7th/cmp-nvim-lsp-signature-help" },
-
-            -- Snippets
-            { "L3MON4D3/LuaSnip" }, -- Required
-            { "rafamadriz/friendly-snippets" }, -- Optional
-        },
-        opts = {
-            ensure_installed = {
-                "vimls",
-                "grammarly",
-                "lua_ls",
-                "rust_analyzer",
-                "pyright",
-                "eslint",
-                "tsserver",
-                "tailwindcss",
-                "html",
-                "jsonls",
-            },
-        },
-        config = function(_, opts)
-            local lsp = require("lsp-zero").preset({
-                name = "minimal",
-                set_lsp_keymaps = true,
-                manage_nvim_cmp = true,
-                suggest_lsp_servers = false,
-            })
-
-            local cmp_sources = lsp.defaults.cmp_sources()
-            table.insert(cmp_sources, { name = "nvim_lsp_signature_help" })
-
-            lsp.setup_nvim_cmp({ sources = cmp_sources })
-
-            lsp.ensure_installed(opts.ensure_installed)
-            -- (Optional) Configure lua language server for neovim
-            lsp.nvim_workspace()
-
-            lsp.setup()
         end,
     },
     {
@@ -334,62 +275,6 @@ return {
         -- end,
     },
     { "kevinhwang91/nvim-hlslens", config = true },
-    {
-        "glepnir/lspsaga.nvim",
-        event = "BufRead",
-        dependencies = {
-            { "nvim-tree/nvim-web-devicons" },
-            { "nvim-treesitter/nvim-treesitter" },
-        },
-        keys = {
-            { "gh", "<cmd>Lspsaga lsp_finder<CR>", desc = "Find Definition & References" },
-            { "gn", "<cmd>Lspsaga rename<CR>", desc = "Rename in File" },
-            { "gN", "<cmd>Lspsaga rename ++project<CR>", desc = "Rename in Project" },
-            { mode = { "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", desc = "Actions" },
-            { "gd", "<cmd>Lspsaga peek_definition<CR>", desc = "Peek Definition" },
-            { "gt", "<cmd>Lspsaga peek_type_definition<CR>", desc = "Peek Type Definition" },
-            { "gT", "<cmd>Lspsaga goto_type_definition<CR>", desc = "Go to Type Definition" },
-            { "go", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "Show Line Diagnostics" },
-            { "gk", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Jump to Prev Diagnostic" },
-            { "gj", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Jump to Next Diagnostic" },
-
-            {
-                "gK",
-                function()
-                    require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
-                end,
-                desc = "Jump to Prev Error",
-            },
-            {
-                "gJ",
-                function()
-                    require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
-                end,
-                desc = "Jump to Next Error",
-            },
-            { "K", "<cmd>Lspsaga hover_doc ++keep<CR>", desc = "Hover Documentation" },
-            { "<leader>co", "<cmd>Lspsaga outline<CR>", desc = "Sorted Code Outline" },
-            { "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>", desc = "Incoming Calls" },
-            { "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>", desc = "Incoming Calls" },
-        },
-        opts = {
-            symbol_in_winbar = {
-                separator = "  ",
-                hide_keyword = true,
-                show_file = false,
-            },
-            ui = {
-                expand = "",
-                collapse = "",
-                preview = " ",
-                code_action = icons.action,
-                diagnostic = icons.bug,
-                incoming = " ",
-                outgoing = " ",
-                hover = " ",
-            },
-        },
-    },
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,60 @@
+return {
+    {
+        "rcarriga/nvim-notify",
+        keys = {
+            {
+                "<leader>un",
+                function()
+                    require("notify").dismiss({ silent = true, pending = true })
+                end,
+                desc = "Delete all Notifications",
+            },
+        },
+        opts = {
+            timeout = 3000,
+            max_height = function()
+                return math.floor(vim.o.lines * 0.75)
+            end,
+            max_width = function()
+                return math.floor(vim.o.columns * 0.75)
+            end,
+        },
+    },
+    {
+        "stevearc/dressing.nvim",
+        lazy = true,
+        init = function()
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- lsp = {
+            --     override = {
+            --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            --         ["vim.lsp.util.stylize_markdown"] = true,
+            --     },
+            -- },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+            },
+        },
+        config = function(_, opts)
+            local noice = require("noice")
+            noice.setup(opts)
+            vim.lsp.handlers["textDocument/hover"] = noice.hover
+            vim.lsp.handlers["textDocument/signatureHelp"] = noice.signature
+        end,
+    },
+}
