42236c82c0afed438acba83858c2eca3c654133e
bartbie <bartbie37@gmail.com>
2023-04-08 02:02:07 +0200

    feat(noice): turned on the lsp markdown features from noice.nvim

--- lua/bartbie/plugins/ui.lua
+++ lua/bartbie/plugins/ui.lua
@@ -38,12 +38,12 @@ return {
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {
-            -- lsp = {
-            --     override = {
-            --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            --         ["vim.lsp.util.stylize_markdown"] = true,
-            --     },
-            -- },
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                },
+            },
             presets = {
                 bottom_search = true,
                 command_palette = true,
