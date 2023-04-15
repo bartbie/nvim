dc1759c6e25d02691b20177dd95fdbf9e4213004
bartbie <bartbie37@gmail.com>
2023-04-15 16:01:19 +0200

    refactor(colorscheme): playing around with kanagawa

--- lua/bartbie/plugins/editor.lua
+++ lua/bartbie/plugins/editor.lua
@@ -4,16 +4,171 @@ local diffview_opened = false
 return {
     {
         "folke/tokyonight.nvim",
+        enabled = false,
         lazy = false,
         priority = 1000,
         opts = {
             style = "night",
-            -- style = "moon",
         },
+        -- opts = function()
+        --     local util = require("tokyonight.util")
+        --     return {
+        --         style = "night",
+        --         -- style = "moon",
+        --         ---@param c ColorScheme "#0db9d7"
+        --         on_colors = function(c)
+        --             -- c.bg = util.lighten(c.black, 0.99)
+        --         end,
+        --
+        --         ---@param hl Highlights
+        --         ---@param c ColorScheme
+        --         on_highlights = function(hl, c)
+        --             -- local prompt = "#2d3149"
+        --
+        --             hl.Type.fg = util.lighten(c.yellow, 0.9)
+        --             hl["@keyword"].fg = util.lighten(c.red, 0.9)
+        --             -- hl["@keyword.return"].fg = c.red
+        --
+        --             -- hl.TelescopeNormal = {
+        --             --     bg = c.bg_dark,
+        --             --     fg = c.fg_dark,
+        --             -- }
+        --             -- hl.TelescopeBorder = {
+        --             --     bg = c.bg_dark,
+        --             --     fg = c.bg_dark,
+        --             -- }
+        --             -- hl.TelescopePromptNormal = {
+        --             --     bg = prompt,
+        --             -- }
+        --             -- hl.TelescopePromptBorder = {
+        --             --     bg = prompt,
+        --             --     fg = prompt,
+        --             -- }
+        --             -- hl.TelescopePromptTitle = {
+        --             --     bg = prompt,
+        --             --     fg = prompt,
+        --             -- }
+        --             -- hl.TelescopePreviewTitle = {
+        --             --     bg = c.bg_dark,
+        --             --     fg = c.bg_dark,
+        --             -- }
+        --             -- hl.TelescopeResultsTitle = {
+        --             --     bg = c.bg_dark,
+        --             --     fg = c.bg_dark,
+        --             -- }
+        --             --
+        --         end,
+        --     }
+        -- end,
         config = function(_, opts)
             local tokyonight = require("tokyonight")
             tokyonight.setup(opts)
             vim.cmd("colorscheme tokyonight")
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        enabled = true,
+        opts = {
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none",
+                        },
+                    },
+                },
+            },
+            ---@param colors { theme: ThemeColors, palette: PaletteColors}
+            overrides = function(colors)
+                local theme = colors.theme
+                local palette = colors.palette
+                return {
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+
+                    -- Save an hlgroup with dark background and dimmed foreground
+                    -- so that you can use it where your still want darker windows.
+                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                    -- Popular plugins that open floats will link to NormalFloat by default;
+                    -- set their background accordingly if you wish to keep them dark and borderless
+                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+                    -- borderless Telescope
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+                    -- dark cmp menu
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+
+                    -- Alpha
+                    AlphaHeader = { fg = palette.lightBlue },
+                    -- AlphaHeader = { fg = palette.lotusTeal2 },
+                    AlphaButtons = { fg = palette.lotusYellow4 },
+                    AlphaShortcut = { fg = palette.oniViolet },
+                    AlphaFooter = { fg = palette.fujiGray },
+
+                    -- cmp kinds
+                    -- Cmp
+                    -- CmpDocumentation = {},
+                    -- CmpDocumentationBorder = {},
+
+                    -- CmpItemAbbr = {},
+                    -- CmpItemAbbrDeprecated = {},
+                    -- CmpItemAbbrMatch = {},
+                    -- CmpItemAbbrMatchFuzzy = {},
+
+                    -- CmpItemMenu = {},
+                    --
+                    -- CmpItemKindDefault = {},
+
+                    CmpItemKindKeyword = { fg = palette.lightBlue },
+
+                    CmpItemKindVariable = { fg = palette.oniViolet },
+                    CmpItemKindConstant = { fg = palette.lotusViolet4 },
+                    CmpItemKindReference = { fg = palette.oniViolet },
+                    -- CmpItemKindValue = {},
+                    CmpItemKindCopilot = { fg = palette.dragonTeal },
+
+                    CmpItemKindFunction = { fg = palette.springBlue },
+                    CmpItemKindMethod = { fg = palette.crystalBlue },
+                    CmpItemKindConstructor = {},
+
+                    CmpItemKindClass = { fg = palette.surimiOrange },
+                    CmpItemKindInterface = { fg = palette.surimiOrange },
+                    CmpItemKindStruct = { fg = palette.surimiOrange },
+                    CmpItemKindEvent = { fg = palette.surimiOrange },
+                    CmpItemKindEnum = { fg = palette.surimiOrange },
+                    CmpItemKindUnit = { fg = palette.surimiOrange },
+
+                    CmpItemKindModule = { fg = palette.autumnYellow },
+
+                    CmpItemKindProperty = { fg = palette.springGreen },
+                    CmpItemKindField = { fg = palette.springGreen },
+                    CmpItemKindTypeParameter = { fg = palette.springGreen },
+                    CmpItemKindEnumMember = { fg = palette.lotusGreen },
+                    CmpItemKindOperator = { fg = palette.springGreen },
+                    CmpItemKindSnippet = { fg = palette.fujiGray },
+                }
+            end,
+        },
+        config = function(_, opts)
+            require("kanagawa").setup(opts)
+            vim.cmd("colorscheme kanagawa")
         end,
     },
     {
@@ -470,5 +625,14 @@ return {
         keys = {
             { "<leader>z", "<CMD>ZenMode<CR>", desc = "zen-mode" },
         },
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function(_, opts)
+            require("colorizer").setup()
+        end,
+    },
+    {
+        "nvim-treesitter/playground",
     },
 }
