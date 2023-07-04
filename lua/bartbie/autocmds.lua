9f0dcb756253867e562102b98051d18901e587d2
bartbie <bartbie37@gmail.com>
2023-07-04 20:04:16 +0200

    feat(autocmds): added more windows to be closed by pressing q

--- lua/bartbie/autocmds.lua
+++ lua/bartbie/autocmds.lua
@@ -52,6 +52,8 @@ vim.api.nvim_create_autocmd("FileType", {
         "spectre_panel",
         "startuptime",
         "tsplayground",
+        "checkhealth",
+        "fugitive",
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
