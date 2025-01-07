00fc1ac6eba419ec276bbcfa7adcdd87165542f3
bartbie <bartbie37@gmail.com>
2025-01-07 18:06:52 +0100

    Adds ui for eob and diagnostics

--- nvim/plugin/options.lua
+++ nvim/plugin/options.lua
@@ -1,5 +1,6 @@
 local o = vim.o
 local g = vim.g
+local opt = vim.opt
 
 g.mapleader = " "
 g.maplocalleader = " "
@@ -47,6 +48,8 @@ o.scrolloff = 1
 o.signcolumn = "yes"
 o.cmdheight = 0
 o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+o.list = true
+opt.listchars:append("eol:󱞤")
 
 -- search-ui
 o.showmatch = true
@@ -55,3 +58,24 @@ o.hlsearch = true
 
 o.termguicolors = vim.fn.has("termguicolors") == 1
 o.lazyredraw = true
+
+local symbols = require("bartbie.symbols")
+
+local symbols_table = {
+    [vim.diagnostic.severity.ERROR] = symbols.diagnostics.error,
+    [vim.diagnostic.severity.WARN] = symbols.diagnostics.warn,
+    [vim.diagnostic.severity.INFO] = symbols.diagnostics.info,
+    [vim.diagnostic.severity.HINT] = symbols.diagnostics.hint,
+}
+
+vim.diagnostic.config({
+    signs = {
+        text = symbols_table,
+    },
+    virtual_text = {
+        prefix = function(d, _, _)
+            return symbols_table[d.severity]
+        end,
+    },
+    severity_sort = true,
+})
