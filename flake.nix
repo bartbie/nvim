6ef96bae698ea0296d3c8bd6294fe10766628f4b
bartbie <bartbie37@gmail.com>
2025-08-23 23:59:34 +0200

    nix: remove flake-utils, use systems instead

--- flake.nix
+++ flake.nix
@@ -3,22 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
-    flake-utils,
     neovim-nightly-overlay,
+    systems,
     ...
   }: let
+    # shortened and dumbened down from github:bartbie/nixos
+    forSystems = systems: fn: let
+      addSystem = sys: (builtins.mapAttrs (_: outp: {${sys} = outp;}));
+    in
+      builtins.foldl' nixpkgs.lib.recursiveUpdate {} (builtins.map (sys: addSystem sys (fn sys)) systems);
+
     # This is where the Neovim derivation is built.
     overlay = import ./nix/overlay.nix {inherit inputs;};
   in
-    flake-utils.lib.eachDefaultSystem
-    (system: let
+    {
+      overlays.default = overlay;
+    }
+    // (forSystems (import systems) (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [overlay];
@@ -27,8 +35,5 @@
       formatter = pkgs.alejandra;
       devShells = import ./nix/shell.nix pkgs;
       packages = import ./nix/packages.nix pkgs;
-    })
-    // {
-      overlays.default = overlay;
-    };
+    }));
 }
