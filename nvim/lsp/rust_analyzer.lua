f6f61dbb4ab352fdc2e7c5a927c53eef22007ca8
bartbie <bartbie37@gmail.com>
2025-08-27 18:18:04 +0200

    lsp: add inlay hints

--- nvim/lsp/rust_analyzer.lua
+++ nvim/lsp/rust_analyzer.lua
@@ -8,6 +8,38 @@ return {
                 command = "clippy",
             },
             checkOnSave = true,
+            inlayHints = {
+                bindingModeHints = {
+                    enable = false,
+                },
+                chainingHints = {
+                    enable = true,
+                },
+                closingBraceHints = {
+                    enable = true,
+                    minLines = 25,
+                },
+                closureReturnTypeHints = {
+                    enable = "never",
+                },
+                lifetimeElisionHints = {
+                    enable = "never",
+                    useParameterNames = false,
+                },
+                maxLength = 25,
+                parameterHints = {
+                    enable = true,
+                },
+                reborrowHints = {
+                    enable = "never",
+                },
+                renderColons = true,
+                typeHints = {
+                    enable = true,
+                    hideClosureInitialization = false,
+                    hideNamedConstructor = false,
+                },
+            },
         },
     },
 }
--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,3 +1,4 @@
+local augroup = require("bartbie.augroup")
 local servers = {
     "lua_ls",
     "ts_ls",
@@ -21,3 +22,19 @@ local servers = {
 for _, name in ipairs(servers) do
     vim.lsp.enable(name)
 end
+
+vim.cmd.highlight("default link LspInlayHint Comment")
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = augroup("lsp_enable_inlay_hints"),
+    callback = function(args)
+        local clients = vim.lsp.get_clients({
+            bufnr = args.buf,
+            method = "textDocument/inlayHint",
+        })
+        if #clients then
+            vim.lsp.inlay_hint.enable(true, {
+                bufnr = args.buf,
+            })
+        end
+    end,
+})
