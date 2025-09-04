7d4d2ce7f80a83baf95809895673b270957e2f95
bartbie <bartbie37@gmail.com>
2025-09-04 19:52:11 +0200

    nvim: init.lua quickfix

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,14 @@
 vim.g.is_nix = vim.g.is_nix or false
 vim.g.is_nix_shim = vim.g.is_nix and vim.g.is_nix_shim or false
 
-local bootstrap = require("bartbie.bootstrap")
+-- block from evaluating bootstrap twice
+if package.loaded["bartbie.bootstrap"] then
+    return
+end
 
-bootstrap.quickenter()
+local bootstrap = require("bartbie.bootstrap")
 
 bootstrap.setup_plugins_folder()
 bootstrap.setup_ftonce_folders()
+
+bootstrap.quickenter()
